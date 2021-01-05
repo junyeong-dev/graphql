436ef654025b2404454b214cac8717486395b57f
Jun-cloud <kjy8827@gmail.com>
2021-01-05 21:14:03 +0900

    Create GraphQL server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 import { GraphQLServer } from "graphql-yoga";
+
+const server = new GraphQLServer({});
+
+// schema는 사용자에게 보내거나 사용자로부처 받을 data에 대한 설명
+server.start(() => console.log("Graphql Server"));
