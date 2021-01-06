3f0788aa4300095a1b29f7b6a82bbb8493f3b3d8
Jun-cloud <kjy8827@gmail.com>
2021-01-06 20:42:38 +0900

    Query and Resolvers

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
 
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers
+});
 
-// schema는 사용자에게 보내거나 사용자로부처 받을 data에 대한 설명
+// schema는 사용자에게 보내거나 사용자로부터 받을 data에 대한 설명
 server.start(() => console.log("Graphql Server"));
+
