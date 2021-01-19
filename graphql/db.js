548080910664ca225de2f2885d2dcfddf3d449f1
Jun-cloud <kjy8827@gmail.com>
2021-01-19 19:58:07 +0900

    Refactoring

--- graphql/db.js
+++ graphql/db.js
@@ -52,40 +52,40 @@ const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
     const {
-        data: {
-          data: { movies }
-        }
+      data: {
+        data: { movies }
+      }
     } = await axios(LIST_MOVIES_URL, {
-        params: {
-            limit,
-            minimum_rating: rating
-        }
-    });
-      return movies;
-};
-
-export const getMovie = async id => {
-    const {
-        data: {
-            data: { movie }
-        }
-    } = await axios(MOVIE_DETAILS_URL, {
-        params: {
-            movie_id: id
-        }
-    });
-    return movie;
-};
-
-export const getSuggestions = async id => {
-    const {
-        data: {
-            data: { movies }
-        }
-    } = await axios(MOVIE_SUGGESTIONS_URL, {
-        params: {
-            movie_id: id
-        }
+      params: {
+        limit,
+        minimum_rating: rating
+      }
     });
     return movies;
-};
+  };
+  
+  export const getMovie = async id => {
+    const {
+      data: {
+        data: { movie }
+      }
+    } = await axios(MOVIE_DETAILS_URL, {
+      params: {
+        movie_id: id
+      }
+    });
+    return movie;
+  };
+  
+  export const getSuggestions = async id => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+      params: {
+        movie_id: id
+      }
+    });
+    return movies;
+  };
--- index.js
+++ index.js
@@ -2,9 +2,24 @@ import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers"
 
 const server = new GraphQLServer({
-    typeDefs: "graphql/schema.graphql",
+    typeDefs: `type Movie {
+      id: Int!
+      title: String!
+      rating: Float
+      description_intro: String
+      language: String
+      medium_cover_image: String
+      genres: [String]
+    }
+    
+    type Query {
+      movies(limit: Int, rating: Float): [Movie]!
+      movie(id: Int!): Movie
+      suggestions(id: Int!): [Movie]!
+    }
+    `,
     resolvers
-});
+  });
 
 // schema는 사용자에게 보내거나 사용자로부터 받을 data에 대한 설명
 server.start(() => console.log("Graphql Server"));
